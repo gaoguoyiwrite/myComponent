056f1199681c8dd409a5ee5994ff47e428c0c552
高兴 <951148962@qq.com>
2022-01-04 11:57:49 +0800

    btn

---
+++ ggy-componnet/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Buton/button'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button disabled>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+        <Button btnType={ButtonType.Link} href="http://baidu.com">Baidu</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
